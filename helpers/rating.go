152196b60326792afd4fee32de60dd527e0b1cff
aldiramdan <aldi.ramdan96@gmail.com>
2023-02-03 19:49:39 +0700

    *

--- helpers/rating.go
+++
@@ -1,9 +0,0 @@
-package helpers
-
-func CalculateNewRating(totalRent int, currentRating, newRating float64) float64 {
-
-	newRating = (currentRating*float64(totalRent) + newRating) / float64(totalRent+1)
-
-	return newRating
-
-}
